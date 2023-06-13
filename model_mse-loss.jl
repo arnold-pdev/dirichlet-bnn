d01b4e00b2a61e6188990193c585a28237e76368
Andrew Arnold <95148925+arnold-pdev@users.noreply.github.com>
2023-06-13 09:06:37 -0600

    Add files via upload
    fake-o_data.jl - file generating data used to create results. uses RNG with unfixed seed.
    model_dirichlet-loss.jl - fits model to data based on Dirichlet loss and generates figures in the style of model_fit_dir_*.png
    model_mse-loss.jl - fits model to data based on MSE loss and generates figures in the style of model_fit_mse_*.png
    DirichletViz.jl - contains functions for creating animations of PDF over the 3-simplex in the style of dirichlet.mp4

---
+++ model_mse-loss.jl
@@ -0,0 +1,59 @@
+using Statistics
+using SpecialFunctions, Distributions
+using Flux, Flux.Optimise
+using Flux: params
+using Base.Iterators: partition
+using LinearAlgebra
+using CUDA
+using Plots
+
+N = 5 # number of nodes in each hidden layer
+
+m = Chain(
+Dense(1 => N, relu),
+Dense(N => N, relu),
+Dense(N => N, relu),
+Dense(N => N, relu),
+Dense(N => N, relu),
+Dense(N => 3), softmax
+) |> gpu
+
+# define loss function
+loss(x, y) = (m(x) - y)'*(m(x) - y) # partial loss
+Loss(x, y) = sum(loss.(x,y)) # total loss
+opt = Adam()
+
+accuracy(x, y) = sum(abs.(norm.(m.(x)./sum.(m.(x)).-y,1)))
+
+epochs = 500 # number of epochs
+# training loop
+for epoch = 1:epochs 
+    for d in train 
+        gs = gradient(params(m)) do 
+            l = Loss(d...)
+        end 
+        update!(opt, params(m), gs)
+    end 
+    # print accuracy wrt validation set
+    @show accuracy(valX, valY)
+    @show Loss(valX, valY)
+end
+
+# plot results
+xt = [ train_x[i][1] for i in eachindex(train_x) ]
+x = 0.0:0.01:1.0
+for j in 1:3
+    # plot mean generating training data
+    μ = [ map_xy(x[i])[j] for i in eachindex(x) ]./([ sum(map_xy(x[i])) for i in eachindex(x) ])
+    plot(x, μ, label = "μ$(j)")
+
+    # plot training data 
+    yt = [ train_y[i][j] for i in eachindex(train_x) ]
+    scatter!(xt, yt, label = "training data")
+
+    # plot model
+    M = [ m([x[i]])[j] for i in eachindex(x) ]
+    plot!(x, M, label = "model")
+
+    savefig("model_fit_mse_$(j).png")
+end
